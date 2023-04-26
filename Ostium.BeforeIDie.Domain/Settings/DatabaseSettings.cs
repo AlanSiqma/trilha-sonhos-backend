38c5eb2b300d8e6ceca5bc28d157574bcab656ae
Alan Siqueira Maia <alan.siqma@gmail.com>
2023-04-26 08:16:06 -0300

    fix update class between dlls

---
+++ Ostium.BeforeIDie.Domain/Settings/DatabaseSettings.cs
@@ -0,0 +1,13 @@
+﻿using Ostium.BeforeIDie.Domain.Contracts.Settings;
+
+namespace Ostium.BeforeIDie.Domain.Settings
+{
+    public class DatabaseSettings: IDatabaseSettings
+    {
+        public string CollectionName { get; set; }
+
+        public string ConnectionString { get; set; }
+
+        public string DatabaseName { get; set; }
+    }
+}
---
+++ Ostium.BeforeIDie.Domain/Settings/EmailTrilhaSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Ostium.BeforeIDie.Domain.Settings
+{
+    public class EmailTrilhaSettings
+    {
+        public string Sender { get; set; }
+
+        public string UrlTrilhaSonhos { get; set; }
+    }
+}
