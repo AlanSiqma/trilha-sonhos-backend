f659c69d520ad5baf28ae14c10ad989aeb7589b8
Alan Siqueira Maia <alan.siqma@gmail.com>
2023-04-28 07:58:25 -0300

    fix add new class library

---
+++ Ostium.BeforeIDie.IoC/AutoMapperConfig.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Ostium.BeforeIDie.Domain.Dto;
+using Ostium.BeforeIDie.Domain.Entities;
+
+namespace Ostium.BeforeIDie.IoC
+{
+    public class AutoMapperConfig:Profile
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<SonhadorEntity, SonhadorDto>().ReverseMap();
+            CreateMap<SonhoEntity, SonhoDto>().ReverseMap();
+            CreateMap<ModeloTrilhaEntity, ModeloTrilhaDto>().ReverseMap();
+            CreateMap<TrilhaEntity, TrilhaDto>().ReverseMap();
+        }
+    }
+}
---
+++ Ostium.BeforeIDie.IoC/InjectionConfigurations.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Ostium.BeforeIDie.Domain.Contracts.Repositories;
+using Ostium.BeforeIDie.Domain.Contracts.Respositories;
+using Ostium.BeforeIDie.Domain.Contracts.Services;
+using Ostium.BeforeIDie.Domain.Model.Contracts.Repositories;
+using Ostium.BeforeIDie.Infra.Data.Repositories;
+
+namespace Ostium.BeforeIDie.IoC
+{
+    public static class InjectionConfigurations
+    {
+        public static IServiceCollection AddInjection(this IServiceCollection services)
+        {
+            services.AddScoped<ISonhadorRepository, SonhadorRepository>();
+            services.AddScoped<ISonhoRepository, SonhoRepository>();
+            services.AddScoped<IModeloTrilhaRepository, ModeloTrilhaRepository>();
+            services.AddScoped<ISolicitacaoResetRepository, SolicitacaoResetRepository>();
+        
+            //services.AddScoped<IEmailService, EmailSender>();
+
+
+            return services;
+        }
+    }
+}
