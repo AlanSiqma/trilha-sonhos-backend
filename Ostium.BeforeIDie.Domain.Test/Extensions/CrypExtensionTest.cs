c16b3b50bd3335507b969b31ab5daaf7140ff312
Alan Siqueira Maia <alan.siqma@gmail.com>
2023-06-01 09:05:49 -0300

    fix add class CrypExtensionTest

---
+++ Ostium.BeforeIDie.Domain.Test/Extensions/CrypExtensionTest.cs
@@ -0,0 +1,21 @@
+﻿using Ostium.BeforeIDie.Domain.Extensions;
+using Xunit;
+namespace Ostium.BeforeIDie.Domain.Test.Extensions
+{
+    public static class CrypExtensionTest
+    {
+        [Fact]
+        public static void Encrypt_ShouldEncryptText()
+        {
+            // Arrange
+            string texto = "aaa";
+            string expectedEncryptedText = "1F9E1D76685D765AA3A6FF85DCED2F0A04F612536DF52696684AAA67787E6CDD";
+
+            // Act
+            string encryptedText = texto.Encrypt();
+
+            // Assert
+            Assert.Equal(expectedEncryptedText, encryptedText);
+        }
+    }
+}
